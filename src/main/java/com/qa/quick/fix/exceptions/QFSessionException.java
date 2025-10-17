7f7ca18a1301510b86bbc78f01ab30a27a94da53
maherkarim <maherkarim96@gmail.com>
2025-10-17 01:30:17 +0100

    t

---
+++ src/main/java/com/qa/quick/fix/exceptions/QFSessionException.java
@@ -0,0 +1,17 @@
+package com.qa.quick.fix.exceptions;
+
+/**
+ * Exception thrown when there is a runtime error related to FIX session state
+ * (e.g., sending on a non-logged-on session, or missing session).
+ */
+public class QFSessionException extends RuntimeException {
+
+    public QFSessionException(String message) {
+        super(message);
+    }
+
+    public QFSessionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
